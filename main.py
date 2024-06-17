f940425eba843bd365a08566484a2aab063a1d72
Zero <john.royer@gmail.com>
2024-06-17 12:40:09 +0800

    blink LED

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-from machine import Pin
-led = Pin(25, Pin.OUT)
-led.value(1)
+# demo code from:
+# https://projects.raspberrypi.org/en/projects/getting-started-with-the-pico/6
+
+from machine import Pin, Timer
+led = Pin(15, Pin.OUT)
+timer = Timer()
+
+def blink(timer):
+    led.toggle()
+
+timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
