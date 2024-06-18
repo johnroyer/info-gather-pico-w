96b3b1d545f222477fec8e1b45f71619965597c3
Zero <john.royer@gmail.com>
2024-06-18 13:45:28 +0800

    change LED blick speed

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from time import sleep
 
 pwm = PWM(Pin(15))
 
-pwm.freq(1000)
+pwm.freq(100)
 
 while True:
-    for duty in range(65025):
+    for duty in range(0, 65025, 1000):
         pwm.duty_u16(duty)
-        sleep(0.0001)
-    for duty in range(65025, 0, -1):
+        sleep(0.01)
+    for duty in range(65025, 0, -1000):
         pwm.duty_u16(duty)
-        sleep(0.0001)
+        sleep(0.01)
