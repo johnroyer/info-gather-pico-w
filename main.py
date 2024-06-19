3b9a99082a8ffbd0e1b1e7e0b0c40ba47f6af8bc
Zero <john.royer@gmail.com>
2024-06-19 16:47:12 +0800

    connect to wifi network
    turn off LED if connect successfully

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from machine import Pin, Timer
 from time import sleep
+import network
 import dht
 
 
@@ -25,6 +26,9 @@ dht_interval = 5
 # power on
 turn_on_led(pin_power)
 
+
+
+
 # wifi not connected
 wifi_led = Pin(pin_wifi, Pin.OUT)
 wifi_led_timer = Timer()
@@ -33,6 +37,27 @@ def blink_wifi_led(timer):
     wifi_led.toggle()
 
 wifi_led_timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink_wifi_led)
+
+# connecting to wifi
+while True:
+    try:
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
+        wlan.connect(net_ssid, net_password)
+
+        if False == wlan.isconnected():
+            print('failed to connect to: ' + net_ssid)
+            sleep(1)
+        else:
+            print('connected to :' + net_ssid)
+            wifi_led_timer.deinit()
+            wifi_led.off()
+            break
+    except KeyboardInterrupt as e:
+        print(e)
+        sleep(2)
+
+
 
 sensor = dht.DHT22(pin_dht22)
 
