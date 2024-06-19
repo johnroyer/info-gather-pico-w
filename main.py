7ead8678e3cc8804a2c305af993c70e5bd8230e8
Zero <john.royer@gmail.com>
2024-06-19 15:59:07 +0800

    set DHT scan interval

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ pin_power = 14
 pin_network = 15
 pin_dht22 = 22
 
+# sensor scan interval in seconds
+dht_interval = 5
+
 lightup_power_led(pin_power)
 
 sensor = dht.DHT22(pin_dht22)
@@ -25,7 +28,7 @@ while 1:
       print("temp: %3.1f " %temp)
       print("Hum: %3.1f %% " %hum)
       
-      sleep(3)
+      sleep(dht_interval)
     except OSError as e:
       print(e)
       sleep(1)
